e751e68a7e090f145c50c616e499ad7755bbfbba
JS <mcgaiver@web.de>
2012-10-26 17:27:29 +0200

    - Anpassen der CloseAction
    Signed-off-by: JS <mcgaiver@web.de>

--- Starfield-Game/src/game/menubar/CloseApplicationAction.java
+++ Starfield-Game/src/game/menubar/CloseApplicationAction.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 /**
  * Schließt die gesamte Applikation
@@ -19,7 +20,7 @@ public class CloseApplicationAction extends AbstractAction {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long	serialVersionUID	= 1L;
 
 	public CloseApplicationAction(String text, ImageIcon icon) {
 		super(text, icon);
@@ -33,7 +34,37 @@ public class CloseApplicationAction extends AbstractAction {
 	 */
 	@Override
 	public void actionPerformed(ActionEvent pE) {
+		// Überprüfen ob nichtgespeicherte Änderungen vorliegen
+		// TODO
+
+		// Confirm Dialog ob Änderungen gespeichert werden sollen
+		final int userChoice = JOptionPane.showConfirmDialog(null,
+				"Wollen Sie das Spiel vor dem Beenden sichern?", "Speichern?",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.INFORMATION_MESSAGE, null);
+
+		switch (userChoice) {
+		case JOptionPane.CANCEL_OPTION:
+			break;
+		case JOptionPane.OK_OPTION:
+			saveGame();
+		default:
+			exitApplication();
+
+		}
+
+	}
+
+	private void exitApplication() {
 		System.exit(0);
 
+	}
+
+	private void saveGame() {
+		// TODO
+		// Speichern des aktuellen Spiels veranlassen
+		System.out.println("Spiel wird gespeichert.");
+		System.out.println("...");
+		System.out.println("Spiel erfolgreich gespeichert.");
 	}
 }
