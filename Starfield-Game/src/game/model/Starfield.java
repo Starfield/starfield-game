adcedcbefae78fcbef239137a0c31fce97678b99
Alexander <alex.Ari@gmx.de>
2012-11-12 23:30:21 +0100

    -im Starfield Methode checkPlayable() implementiert, die die Spielbarkeit des entworfenen Spieles testen soll. Noch keine Eindeutigkeitsüberprüfung!
    Signed-off-by: Alexander <alex.Ari@gmx.de>

--- Starfield-Game/src/game/model/Starfield.java
+++ Starfield-Game/src/game/model/Starfield.java
@@ -200,81 +200,362 @@ public class Starfield implements Serializable {
 		}
 		return this;
 	}
- /**
-  *  Diese Methode gibt alle Felder zurück, auf die ein Pfeil zeigt.
-  */
-	private ArrayList<Field> getFieldsfromArrowDirection(Field field){
-		ArrayList<Field> pointedFields = new ArrayList<Field>();
+
+	/**
+	 * Diese Methode gibt eine ArrayList mit Argumenten zurück: An Pos. 0 Liste
+	 * mit Felder, auf die ein Pfeil zeigt oder an Pos 1-8 ArrayListen mit
+	 * Feldern um ein Stern rum in der jeweiligen Richtung.
+	 * (UL,U,UR,R,DR,D,DL,L)
+	 */
+	private ArrayList<ArrayList<Field>> getFieldsInDirectionFromStarOrArrow(
+			Field field) {
+		ArrayList<ArrayList<Field>> args = new ArrayList<ArrayList<Field>>();
+		ArrayList<Field> pointedFieldsFromArrow = new ArrayList<Field>();
+		args.add(pointedFieldsFromArrow);
+		ArrayList<Field> pointedFieldsFromStarUL = new ArrayList<Field>();
+		ArrayList<Field> pointedFieldsFromStarU = new ArrayList<Field>();
+		ArrayList<Field> pointedFieldsFromStarUR = new ArrayList<Field>();
+		ArrayList<Field> pointedFieldsFromStarR = new ArrayList<Field>();
+		ArrayList<Field> pointedFieldsFromStarDR = new ArrayList<Field>();
+		ArrayList<Field> pointedFieldsFromStarD = new ArrayList<Field>();
+		ArrayList<Field> pointedFieldsFromStarDL = new ArrayList<Field>();
+		ArrayList<Field> pointedFieldsFromStarL = new ArrayList<Field>();
+		args.add(pointedFieldsFromStarUL);
+		args.add(pointedFieldsFromStarU);
+		args.add(pointedFieldsFromStarUR);
+		args.add(pointedFieldsFromStarR);
+		args.add(pointedFieldsFromStarDR);
+		args.add(pointedFieldsFromStarD);
+		args.add(pointedFieldsFromStarDL);
+		args.add(pointedFieldsFromStarL);
 		switch (field.getUserContent()) {
 		case CONTENT_STAR:
-			
+			Field field2 = field;
+			while (!(getField_UL(field2) == null)) {
+				field2 = getField_UL(field2);
+				pointedFieldsFromStarUL.add(field2);
+			}
+			field2 = field;
+			while (!(getField_U(field2) == (null))) {
+				field2 = getField_U(field2);
+				pointedFieldsFromStarU.add(field2);
+			}
+			field2 = field;
+			while (!(getField_UR(field2) == (null))) {
+				field2 = getField_UR(field2);
+				pointedFieldsFromStarUR.add(field2);
+			}
+			field2 = field;
+			while (!(getField_L(field2) == (null))) {
+				field2 = getField_L(field2);
+				pointedFieldsFromStarL.add(field2);
+			}
+			field2 = field;
+			while (!(getField_R(field2) == (null))) {
+				field2 = getField_R(field2);
+				pointedFieldsFromStarR.add(field2);
+			}
+			field2 = field;
+			while (!(getField_DL(field2) == (null))) {
+				field2 = getField_DL(field2);
+				pointedFieldsFromStarDL.add(field2);
+			}
+			field2 = field;
+			while (!(getField_D(field2) == (null))) {
+				field2 = getField_D(field2);
+				pointedFieldsFromStarD.add(field2);
+			}
+			field2 = field;
+			while (!(getField_DR(field2) == (null))) {
+				field2 = getField_DR(field2);
+				pointedFieldsFromStarDR.add(field2);
+			}
 			break;
 		case CONTENT_EMPTY:
-			
 			break;
 		case CONTENT_GRAYED:
-			
 			break;
 		case CONTENT_ARROW_UL:
-				
-					
-			
-			
+			while (!(getField_UL(field) == (null))) {
+				field = getField_UL(field);
+				pointedFieldsFromArrow.add(field);
+			}
 			break;
 		case CONTENT_ARROW_U:
-			
+			while (!(getField_U(field) == (null))) {
+				field = getField_U(field);
+				pointedFieldsFromArrow.add(field);
+			}
 			break;
 		case CONTENT_ARROW_UR:
-			
+			while (!(getField_UR(field) == (null))) {
+				field = getField_UR(field);
+				pointedFieldsFromArrow.add(field);
+			}
 			break;
 		case CONTENT_ARROW_L:
-			
+			while (!(getField_L(field) == (null))) {
+				field = getField_L(field);
+				pointedFieldsFromArrow.add(field);
+			}
 			break;
 		case CONTENT_ARROW_R:
-			
+			while (!(getField_R(field) == (null))) {
+				field = getField_R(field);
+				pointedFieldsFromArrow.add(field);
+			}
 			break;
 		case CONTENT_ARROW_DL:
-			
+			while (!(getField_DL(field) == (null))) {
+				field = getField_DL(field);
+				pointedFieldsFromArrow.add(field);
+			}
 			break;
 		case CONTENT_ARROW_D:
-			
+			while (!(getField_D(field) == (null))) {
+				field = getField_D(field);
+				pointedFieldsFromArrow.add(field);
+			}
+			break;
+		case CONTENT_ARROW_DR:
+			while (!(getField_DR(field) == (null))) {
+				field = getField_DR(field);
+				pointedFieldsFromArrow.add(field);
+			}
 			break;
 		}
-		
-		return pointedFields;
+
+		return args;
 	}
-		
-	
-	    /**
-	     * Gibt das Feld linksoben vom übergebenen Feld aus.
-	     * @return
-	     */
-		public Field getField_UL(Field field){
-			if(field.getWidth()>0 && field.getHeight()>0){
-				return listcontainer.get(field.getWidth()-1).get(field.getHeight()-1);					
-			}
-				return null;
+
+	/**
+	 * Gibt das Feld linksoben vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_UL(Field field) {
+		if (field.getxPos() > 0 && field.getyPos() > 0) {
+			return listcontainer.get(field.getxPos() - 1).get(
+					field.getyPos() - 1);
 		}
-		
-		/**
-		 * Gibt das Feld oben vom übergebenen Feld aus.
-		 * @return
-		 */
-		public Field getField_U(Field field){
-			if(field.getHeight()>0){
-				return listcontainer.get(field.getWidth()).get(field.getHeight()-1);					
-			}
-				return null;
+		return null;
+	}
+
+	/**
+	 * Gibt das Feld oben vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_U(Field field) {
+		if (field.getyPos() > 0) {
+			return listcontainer.get(field.getxPos()).get(field.getyPos() - 1);
 		}
-	    /**
-	     * Gibt das Feld rechtsoben vom übergebenen Feld aus.
-	     * @return
-	     */
-		public Field getField_UR(Field field){
-			if(field.getWidth()>=listcontainer.size() && field.getHeight()>0){
-				return listcontainer.get(field.getWidth()+1).get(field.getHeight()-1);					
-			}
-				return null;
+		return null;
+	}
+
+	/**
+	 * Gibt das Feld rechtsoben vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_UR(Field field) {
+		if (field.getxPos() < (int) size.getSize().getWidth() - 1
+				&& field.getyPos() > 0) {
+			return listcontainer.get(field.getxPos() + 1).get(
+					field.getyPos() - 1);
 		}
-		
+		return null;
+	}
+
+	/**
+	 * Gibt das Feld rechts vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_R(Field field) {
+		if (field.getxPos() < (int) size.getSize().getWidth() - 1) {
+			return listcontainer.get(field.getxPos() + 1).get(field.getyPos());
+		}
+		return null;
+	}
+
+	/**
+	 * Gibt das Feld rechtsunten vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_DR(Field field) {
+		if (field.getxPos() < (int) size.getSize().getWidth() - 1
+				&& field.getyPos() < (int) size.getSize().getHeight() - 1) {
+			return listcontainer.get(field.getxPos() + 1).get(
+					field.getyPos() + 1);
+		}
+		return null;
+	}
+
+	/**
+	 * Gibt das Feld unten vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_D(Field field) {
+		if (field.getyPos() < (int) size.getSize().getHeight() - 1) {
+			return listcontainer.get(field.getxPos()).get(field.getyPos() + 1);
+		}
+		return null;
+	}
+
+	/**
+	 * Gibt das Feld linksunten vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_DL(Field field) {
+		if (field.getxPos() > 0
+				&& field.getyPos() < (int) size.getSize().getHeight() - 1) {
+			return listcontainer.get(field.getxPos() - 1).get(
+					field.getyPos() + 1);
+		}
+		return null;
+	}
+
+	/**
+	 * Gibt das Feld links vom übergebenen Feld aus.
+	 * 
+	 * @return
+	 */
+	public Field getField_L(Field field) {
+		if (field.getxPos() > 0) {
+			return listcontainer.get(field.getxPos() - 1).get(field.getyPos());
+		}
+		return null;
+	}
+
+	/**
+	 * Testet ob ein Stern in einer Reine/ Arrayliste liegt.
+	 * 
+	 * @return
+	 */
+	private Boolean ContainsStar(ArrayList<Field> directionFieldList) {
+		for (Field f : directionFieldList) {
+			if (f.getUserContent().equals(AllowedContent.CONTENT_STAR)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Testet ob der Stern von einem entsprechenden Arrow getroffen wird.
+	 * 
+	 * @return
+	 */
+	private Boolean IsHitByArrow(ArrayList<Field> FieldList,
+			AllowedContent content) {
+		switch (content) {
+		case CONTENT_ARROW_UL:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_UL)) {
+					return true;
+				}
+			}
+			break;
+		case CONTENT_ARROW_U:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_U)) {
+					return true;
+				}
+			}
+			break;
+		case CONTENT_ARROW_UR:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_UR)) {
+					return true;
+				}
+			}
+			break;
+		case CONTENT_ARROW_L:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_L)) {
+					return true;
+				}
+			}
+			break;
+		case CONTENT_ARROW_R:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_R)) {
+					return true;
+				}
+			}
+			break;
+		case CONTENT_ARROW_DL:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_DL)) {
+					return true;
+				}
+			}
+			break;
+		case CONTENT_ARROW_D:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_D)) {
+					return true;
+				}
+			}
+			break;
+		case CONTENT_ARROW_DR:
+			for (Field f : FieldList) {
+				if (f.getUserContent().equals(AllowedContent.CONTENT_ARROW_DR)) {
+					return true;
+				}
+			}
+			break;
+
+		}
+		return false;
+	}
+
+	public boolean checkPlayable() {
+		ArrayList<ArrayList<Field>> args = new ArrayList<ArrayList<Field>>();
+		for (int x = 0; x < size.getWidth(); x++) {
+			for (int y = 0; y < size.getHeight(); y++) {
+				args = getFieldsInDirectionFromStarOrArrow(listcontainer.get(x)
+						.get(y)); // Rückgabe der Argument ArrayListen
+				if (!(args.get(0).isEmpty()
+						&& args.get(3).isEmpty() && args.get(6)
+								.isEmpty())) { // leeres Feld !!!
+					if (args.get(3).isEmpty() && args.get(6)
+							.isEmpty()) {// Pfeil
+						if (!ContainsStar(args.get(0))) { // Checkt ob der Pfeil
+															// auf einen Stern
+															// zeigt
+							return false;
+						}
+					} else {
+						if (!(IsHitByArrow(args.get(1),
+								AllowedContent.CONTENT_ARROW_DR))
+								&& !(IsHitByArrow(args.get(2),
+										AllowedContent.CONTENT_ARROW_D))
+								&& !(IsHitByArrow(args.get(3),
+										AllowedContent.CONTENT_ARROW_DL))
+								&& !(IsHitByArrow(args.get(4),
+										AllowedContent.CONTENT_ARROW_L))
+								&& !(IsHitByArrow(args.get(5),
+										AllowedContent.CONTENT_ARROW_UL))
+								&& !(IsHitByArrow(args.get(6),
+										AllowedContent.CONTENT_ARROW_U))
+								&& !(IsHitByArrow(args.get(7),
+										AllowedContent.CONTENT_ARROW_UR)) && !(IsHitByArrow(
+									args.get(8), AllowedContent.CONTENT_ARROW_R))) {
+							return false;
+						}
+					}
+
+				}
+			}
+		}
+
+		return true;
+
+	}
+
 }
